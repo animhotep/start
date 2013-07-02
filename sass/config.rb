45b06e347d0d7fce8ea772beda6ca2b2d7f2f16c
Your Name <you@example.com>
2013-07-02 10:07:05 +0300

    sass config for sublime

--- sass/config.rb
+++ sass/config.rb
@@ -1,8 +1,8 @@
 http_path = "/"
-css_dir = "."
+css_dir = "../css"
 sass_dir = "."
-images_dir = "img"
-javascripts_dir = "js"
-output_style = :compressed
+images_dir = "../img"
+javascripts_dir = "../js"
+# output_style = :expanded or :nested or :compact or :compressed
 relative_assets=true
 line_comments = false
